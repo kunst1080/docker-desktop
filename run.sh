4ce6e213ec2c9c4b070d1ee1331fc03af64aba25
kunst1080 <kontrapunkt1080@gmail.com>
2018-02-13 22:16:40 +0900

    change mount setting for dbus and systemd

--- run.sh
+++ run.sh
@@ -25,10 +25,11 @@ volumes-X11() {
 }
 
 exec docker run --rm --privileged \
+    --shm-size=2gb \
     -e LANG=$LANG \
     -v $PWD/xinitrc.docker:/home/$USER/.xinitrc.docker \
     -v $PWD/install-in-the-container:/home/$USER/local/install \
-    $(volumes /dev/shm /run/udev /run/dbus/system_bus_socket /var/run/docker.sock) \
+    $(volumes /run/udev /run/dbus /run/systemd /var/run/docker.sock) \
     $(volumes-ro /etc/localtime /etc/locale.gen /usr/lib/locale/locale-archive) \
     $(volumes-X11 .config .ssh .mozc local work Downloads Pictures .zsh_history .gitconfig.local .todoist.config.json) \
     $IMAGE $* | tee xsession.log
